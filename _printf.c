4b811b9c5c17980f3e13daf490e941fb1bf2e8e3
lbayenew1 <asterbahran@gmail.com>
2022-10-18 10:20:53 -0700

    creating printf

--- _printf.c
+++ _printf.c
@@ -17,7 +17,6 @@ int _printf(const char *format, ...)
 		{"i", print_integer},
 		{"b", print_binary},
 		{"r", print_reversed},
-		{"R", rot13},
 		{"u", unsigned_integer},
 		{"o", print_octal},
 		{"x", print_hex},
